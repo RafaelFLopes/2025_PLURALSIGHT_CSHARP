c61638cb2ffa4967f6ab18ba19c0dff75fd5fa11
RafaelFLopes <ferreiralopesrafael109@gmail.com>
2026-01-12 00:54:14 -0300

    finalizando o modulo 13

--- CSHARP Fundamentals/modulo 13 - Reading from and Writing to Files/BethanysPieShopHRM/BethanysPieShopHRM/Utilities.cs
+++ CSHARP Fundamentals/modulo 13 - Reading from and Writing to Files/BethanysPieShopHRM/BethanysPieShopHRM/Utilities.cs
@@ -154,7 +154,87 @@ namespace BethanysPieShopHRM
 
         internal static void LoadEmployees(List<Employee> employees)
         {
-            
+            string path = $"{directory}\\{fileName}";
+
+            if(File.Exists(path)){
+                employees.Clear();
+
+                //now read the file
+
+                string[] employeesAsString = File.ReadAllLines(path);
+
+                for(int i = 0; i < employeesAsString.Length; i++){
+                    string[] employeeSplit = employeesAsString[i].Split(';');
+
+                    string firstName = employeeSplit[0].Substring(employeeSplit[0].IndexOf(':') + 1);
+                    string lastName = employeeSplit[1].Substring(employeeSplit[1].IndexOf(':') + 1);
+                    string email = employeeSplit[2].Substring(employeeSplit[2].IndexOf(':') + 1);
+                    DateTime birthDay = DateTime.Parse(employeeSplit[3].Substring(employeeSplit[3].IndexOf(':') + 1));
+                    double hourlyRate = double.Parse(employeeSplit[4].Substring(employeeSplit[4].IndexOf(':') + 1));
+                    string employeeType = employeeSplit[5].Substring(employeeSplit[5].IndexOf(':') + 1);
+
+                    Employee employee = null;
+
+                    switch (employeeType)
+                    {
+                        case "1":
+                            employee = new Employee
+                                (
+                                firstName,
+                                lastName,
+                                email,
+                                birthDay,
+                                hourlyRate
+                            );
+                            break;
+                        case "2":
+                            employee = new Manager
+                            (
+                                firstName,
+                                lastName,
+                                email,
+                                birthDay,
+                                hourlyRate
+                            );
+                            break;
+                        case "3":
+                            employee = new StoreManager
+                            (
+                                firstName,
+                                lastName,
+                                email,
+                                birthDay,
+                                hourlyRate
+                            );
+                            break;
+                        case "4":
+                            employee = new Researcher
+                            (
+                                firstName,
+                                lastName,
+                                email,
+                                birthDay,
+                                hourlyRate
+                            );
+                            break;
+                        case "5":
+                            employee = new JuniorResearcher
+                            (
+                                firstName,
+                                lastName,
+                                email,
+                                birthDay,
+                                hourlyRate
+                            );
+                            break;
+                    }
+
+                    employees.Add(employee);
+
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Loaded {employees.Count} employees!");
+            }
         }
         internal static void CheckForExistingEmployeeFile()
         {
--- CSHARP Fundamentals/modulo 13 - Reading from and Writing to Files/BethanysPieShopHRM/BethanysPieShopHRM/obj/Debug/net8.0/BethanysPieShopHRM.AssemblyInfo.cs
+++ CSHARP Fundamentals/modulo 13 - Reading from and Writing to Files/BethanysPieShopHRM/BethanysPieShopHRM/obj/Debug/net8.0/BethanysPieShopHRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+434f659edf45e10379f02022fe3203da8c88420d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a76ff85ae22482df6cfbac33e08b64cc669bee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
